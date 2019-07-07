38710dd18381e6a65e0a06df45cd9d3f15d73030
Manuel Cantu Reinhard <m.cantu.reinhard@gmail.com>
2019-07-08 01:08:50 +0200

    Implement roman to_decimal

--- src/main/java/com/example/application/components/numberConverter/implementations/Roman.java
+++ src/main/java/com/example/application/components/numberConverter/implementations/Roman.java
@@ -3,12 +3,46 @@ package com.example.application.components.numberConverter.implementations;
 import com.example.application.components.numberConverter.NumberConverterBaseClass;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 @Component
 public class Roman extends NumberConverterBaseClass {
 
     @Override
     public Long to_decimal(String value) {
-        return (long)0;
+        if(value.length() == 0){
+            return (long)0;
+        }
+        Map<Character, Integer> valMap = new HashMap<Character, Integer>(){{
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
+        }};
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < value.length(); i++){
+            char current = value.charAt(i);
+            int numeric = valMap.get(current);
+            if(st.empty()){
+                st.push(numeric);
+            } else {
+                int sub = 0;
+                while(!st.empty() && numeric > st.peek()){
+                    sub += st.pop();
+                }
+                st.push(numeric - sub);
+            }
+        }
+        int res = 0;
+        while(!st.empty()){
+            res += st.pop();
+        }
+        return (long)res;
     }
     @Override
     public String from_decimal(Long value) {
