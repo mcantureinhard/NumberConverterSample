930e8b43bca4e5585e6588367581250648cf72e9
Manuel Cantu Reinhard <m.cantu.reinhard@gmail.com>
2019-07-07 19:53:52 +0200

    Wire AvailableConvertersService to our use case

--- src/main/java/com/example/application/usecases/AvailableConversionsUseCase.java
+++ src/main/java/com/example/application/usecases/AvailableConversionsUseCase.java
@@ -1,13 +1,16 @@
 package com.example.application.usecases;
 
+import com.example.application.services.AvailableConvertersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class AvailableConversionsUseCase {
+    @Autowired
+    AvailableConvertersService availableConvertersService;
     public List<String> run() {
-        return new ArrayList<>();
+        return availableConvertersService.getConvertersNames();
     }
 }
