3a43ac48f58542e13fdc281cd2d0d331b9dce729
Manuel Cantu Reinhard <m.cantu.reinhard@gmail.com>
2019-07-07 19:42:20 +0200

    Finish implementing test feature steps

--- src/bdd/java/com/example/bdd/steps/ConverterSteps.java
+++ src/bdd/java/com/example/bdd/steps/ConverterSteps.java
@@ -1,27 +1,40 @@
 package com.example.bdd.steps;
 
+import com.example.application.usecases.AvailableConversionsUseCase;
 import com.example.bdd.BaseIntegrationTest;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 public class ConverterSteps extends BaseIntegrationTest {
+    HashMap<String, Object> dataHolder = new HashMap<>();
+    Boolean lastCheck = false;
+
+    @Autowired
+    AvailableConversionsUseCase availableConversionsUseCase;
+
     @Given("^the list of providers$")
     public void the_list_of_providers() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        List<String> availableConversions = availableConversionsUseCase.run();
+        dataHolder.put("availableConversions", availableConversions);
     }
 
     @When("^I check \"([^\"]*)\" is there$")
-    public void i_check_is_there(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void i_check_is_there(String name) throws Throwable {
+        List<String> availableConversion = (List<String>) dataHolder.get("availableConversions");
+        lastCheck = availableConversion.contains(name);
     }
 
     @Then("^It should be true$")
     public void it_should_be_true() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        assertTrue(lastCheck);
+        lastCheck = false;
     }
 }
