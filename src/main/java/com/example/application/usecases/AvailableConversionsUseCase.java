a1052e15df40bd638fe9841eef25cb59a1325852
Manuel Cantu Reinhard <m.cantu.reinhard@gmail.com>
2019-07-07 19:39:20 +0200

    Add use case for available conversions

---
+++ src/main/java/com/example/application/usecases/AvailableConversionsUseCase.java
@@ -0,0 +1,13 @@
+package com.example.application.usecases;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AvailableConversionsUseCase {
+    public List<String> run() {
+        return new ArrayList<>();
+    }
+}
